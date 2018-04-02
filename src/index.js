cdd73dd502a333f2de921363b19f4135cf46854c
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2018-04-02 11:52:09 -0500

    Resized and converted to jpg all projects images

--- src/index.js
+++ src/index.js
@@ -83,57 +83,68 @@ class ProjectsView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      projects: [{
-        "name": "360 PHOTO TO VIDEO",
-        "description": "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo.",
-        "img": "img/360photo-to-video.png",
-        "link": "360-photo-to-video"
-      }, {
-        "name": "GENDERGRAPH",
-        "description": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim.",
-        "img": "img/gendergraph.png",
-        "link": "gendergraph"
-      }, {
-        "name": "STREET SOUNDS",
-        "description": "Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi.",
-        "img": "img/street-sonds.png",
-        "link": "https://streetsounds.tk"
-      }, {
-        "name": "SUBCITY",
-        "description": "Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
-        "img": "img/subcity.png",
-        "link": "http://subcity.tk"
-      }, {
-        "name": "TERMINAL PRINTS",
-        "description": "Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
-        "img": "img/terminal-prints.png",
-        "link": "terminal-prints.html"
-      }, {
-        "name": "VERTEX CACHE",
-        "description": "Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.",
-        "img": "img/vertex-cache.png",
-        "link": "vertex-cache"
-      }, {
-        "name": "WHIRPOOLS",
-        "description": "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
-        "img": "img/whirpools.png ",
-        "link": "whirlpools.html"
-      }, {
-        "name": "PHOTOSPHERES",
-        "description": "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
-        "img": "img/photospheres.png ",
-        "link": "photospheres.html"
-      }, {
-        "name": "3D TSP HEART",
-        "description": "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
-        "img": "img/tsp-3d-art.png ",
-        "link": "tsp-3d-art"
-      }, {
-	"name": "THREEJS CURVATURE",
-	"description": "we're famous now i<3 mr.doob",
-	"img": "img/curvatureExample.jpg",
-	"link": "https://threejs.org/examples/#webgl_materials_curvature"
-      }]
+      projects: [
+        {
+          "name": "STREET SOUNDS",
+          "description": "Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi.",
+          "img": "img/street-sonds.jpg",
+          "link": "https://streetsounds.tk"
+        },
+        {
+          "name": "THREEJS CURVATURE",
+          "description": "we're famous now i<3 mr.doob",
+          "img": "img/curvatureExample.jpg",
+          "link": "https://threejs.org/examples/#webgl_materials_curvature"
+        },
+        {
+          "name": "3D TSP HEART",
+          "description": "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+          "img": "img/tsp-3d-art.jpg ",
+          "link": "tsp-3d-art"
+        },
+        {
+          "name": "360 PHOTO TO VIDEO",
+          "description": "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo.",
+          "img": "img/360photo-to-video.jpg",
+          "link": "360-photo-to-video"
+        },
+        {
+          "name": "VERTEX CACHE",
+          "description": "Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.",
+          "img": "img/vertex-cache.jpg",
+          "link": "vertex-cache"
+        },
+        {
+          "name": "WHIRPOOLS",
+          "description": "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+          "img": "img/whirpools.jpg ",
+          "link": "whirlpools.html"
+        },
+        {
+          "name": "GENDERGRAPH",
+          "description": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim.",
+          "img": "img/gendergraph.jpg",
+          "link": "gendergraph"
+        },
+        {
+          "name": "PHOTOSPHERES",
+          "description": "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+          "img": "img/photospheres.jpg ",
+          "link": "photospheres.html"
+        },
+        {
+          "name": "TERMINAL PRINTS",
+          "description": "Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+          "img": "img/terminal-prints.jpg",
+          "link": "terminal-prints.html"
+        },
+        {
+          "name": "SUBCITY",
+          "description": "Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+          "img": "img/subcity.jpg",
+          "link": "http://subcity.tk"
+        }
+      ]
     }
   }
   render() {
