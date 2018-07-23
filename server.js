11123fd0d8da9c5bac958c50725829e4de161663
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2018-07-23 11:27:57 +0300

    Added express routes for frontend

--- server.js
+++ server.js
@@ -9,36 +9,47 @@ const subApps = [{
   name: 'vertexCache',
   entry: 'repos/vertex-texture-cache/public/html/index.html',
   public: ['repos/vertex-texture-cache/public'],
-  route: '/vertex-cache'
+  route: '/vertex-cache',
 }, {
   name: 'auracariaDreams',
   entry: 'repos/auracaria-dreams/public/html/mary.html',
   public: ['repos/auracaria-dreams/public'],
-  route: '/auracaria-dreams'
+  route: '/auracaria-dreams',
 }, {
   name: 'genderGraph',
   entry: 'repos/gender-graph/public/html/index.html',
   public: ['repos/gender-graph/public'],
-  route: '/gendergraph'
+  route: '/gendergraph',
 }, {
   name: 'photoToVideo',
   entry: 'repos/360-photo-to-video/public/html/index.html',
   public: ['repos/360-photo-to-video/public'],
-  route: '/360-photo-to-video'
+  route: '/360-photo-to-video',
 }, {
   name: 'tsp3dArt',
   entry: 'repos/tsp-3D-art/public/html/final.html',
   public: ['repos/tsp-3D-art/public'],
-  route: '/tsp-3D-art'
+  route: '/tsp-3D-art',
 }];
 
 codercat.use(express.static('build'));
 codercat.use(express.static('build/html'));
 
-subApps.forEach(app => {
-  let expressApp = express();
+// Frontend routes for react router
+codercat.get('/gallery', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+codercat.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+codercat.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-  for (let i=0; i<app.public.length; i++) {
+subApps.forEach((app) => {
+  const expressApp = express();
+
+  for (let i = 0; i < app.public.length; i += 1) {
     expressApp.use(express.static(app.public[i]));
   }
 
