6d2975f5fd29b2fed5c99f463987567403191072
Sneha Belkhale <sbelkhale@yahoo.com>
2019-05-05 15:05:57 +0900

    adding about description

--- src/About.js
+++ src/About.js
@@ -10,14 +10,10 @@ export default function About() {
           About
         </h1>
         <p>
-          Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus.
-          Aliquam sit amet diam in magna bibendum imperdiet.
-          Nullam orci pede, venenatis non,
-          sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus.
-          Morbi sem mauris, laoreet ut,rhoncus aliquet, pulvinar sed, nisl.
-          Nunc rhoncus dui vel sem. Sed sagittis.
-          Nam congue, risussemper porta volutpat, quam pede lobortis ligula,
-          sit amet eleifend pede libero quis orci.
+          We are a duo of 3D Graphic enthusiasts, building experiences that advance the state of graphics on the web.
+          Our areas of interest include WebVR, Procedural Animations, and using technology to bring new persepectives on social issues.
+          <br /><br />
+          Contact us for collaborations~
         </p>
       </div>
     </div>
