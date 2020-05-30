d2edaa7d3c28148497b9513446ab0c2457b78f9b
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2020-05-29 18:26:42 -0700

    Colored project overlayes

--- src/components/ProjectsView/index.js
+++ src/components/ProjectsView/index.js
@@ -3,23 +3,39 @@ import projects from '../../constants/projects';
 import s from './index.module.css';
 
 const serverURL = process.env.REACT_APP_CODERCAT_SERVER_URL || '';
-const makeProjectLink = route => route.startsWith('http') ? route : `${serverURL}/${route}`;
+const makeProjectLink = (route) =>
+  route.startsWith('http') ? route : `${serverURL}/${route}`;
 
 const ProjectsView = () => {
+  // Coder cat logo colors
+  const overlayColors = ['#ee7a39', '#fc9827', '#f7d72d', '#28d3c2', '#31b2c3'];
+
+  const projectElements = projects.map((project) => {
+    const colorIndex = Math.floor(Math.random() * overlayColors.length);
+    const overlayColor = overlayColors[colorIndex];
+
+    return (
+      <a
+        id="project-box"
+        href={makeProjectLink(project.route)}
+        key={project.name}
+        className={s.projectLink}
+      >
+        <div className={s.overlay} style={{ backgroundColor: overlayColor }}>
+          <h1 className={s.projectTitle}>{project.name}</h1>
+        </div>
+        <img className={s.img} src={project.img} alt={project.name} />
+      </a>
+    );
+  });
+
   return (
     <div className="view" id="projects-view">
-      <div className="main-container" id="project-container">
-        {projects.map(project => (
-          <a id="project-box" href={makeProjectLink(project.route)} key={project.name}>
-            <img src={project.img} alt={project.name} />
-            <h1 id="box-title">
-              {project.name}
-            </h1>
-          </a>
-        ))}
+      <div className={s.projectBox} id="project-container">
+        {projectElements}
       </div>
     </div>
   );
-}
+};
 
 export default ProjectsView;
